e64837f73daae9e1aec5deb5c1c212b8105eb9a9
ulwlu <ooulwluoo@gmail.com>
2020-11-22 16:48:58 +0900

    Add simple bar as status bar

---
+++ dotfiles/Library/Application Support/Übersicht/widgets/simple-bar/lib/styles/Colors.js
@@ -0,0 +1,19 @@
+export const Colors = {
+  main: "#040F18",
+  minor: "#48697E",
+  accent: "#277BB1",
+  red: "#1F6CA1",
+  green: "#277BB1",
+  yellow: "#a0cce2",
+  orange: "#1F6CA1",
+  blue: "#277BB1",
+  magenta: "#10598B",
+  cyan: "#48697E",
+  background: "#040F18",
+  lightGrey: 'rgba(0, 0, 0, 0.05)',
+  font: 'HackNerdFontComplete',
+  lightShadow: '0 5px 10px rgba(0,0,0,0.24)',
+  mediumShadow: '0 8px 30px rgba(0,0,0,0.24)',
+  largeShadow: '0 30px 60px rgba(0,0,0,0.24)',
+  easing: 'cubic-bezier(0.4, 0, 0.2, 1)'
+}
--- dotfiles/Library/Application Support/Übersicht/widgets/status_bar/helpers.jsx
+++
@@ -1,7 +0,0 @@
-export const parse = input => {
-  try {
-    return JSON.parse(input);
-  } catch (e) {
-    return undefined;
-  }
-};
